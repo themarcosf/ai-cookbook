c0793b703da987a577b24bbbc978791b1ac93c67
Marcos Florencio <marcosaurelio.adm@gmail.com>
2023-10-15 13:18:43 -0300

    rm langchain

--- langchain/src/chains/chains.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChainsService } from './chains.service';
-import { ChainsController } from './chains.controller';
-
-@Module({
-  controllers: [ChainsController],
-  providers: [ChainsService],
-})
-export class ChainsModule {}
--- langchain/src/model-io/model-io.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ModelIoService } from './model-io.service';
-import { ModelIoController } from './model-io.controller';
-
-@Module({
-  controllers: [ModelIoController],
-  providers: [ModelIoService],
-})
-export class ModelIoModule {}
