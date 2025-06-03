553cfbc7713cfca0344121d2754ca3e0b6ffd2b7
EC2 Default User <ec2-user@ip-172-31-40-243.ec2.internal>
2025-06-03 23:34:57 +0000

    .

--- karpathy/gpt2-edu_fineweb10B.py
+++ karpathy/gpt2-edu_fineweb10B.py
@@ -11,34 +11,45 @@ Will save shards to the local directory 'edu_fineweb10B_shards'.
 """
 import multiprocessing as mp
 import os
+import pickle
 
-from datasets import load_dataset
 import numpy as np
 import tiktoken
+import torch
+from datasets import load_dataset
 from tqdm import tqdm
 
-local_dir = 'edu_fineweb10B_shards'
+shards_dir = 'edu_fineweb10B_shards'
+encodings_dir = 'seed_encodings'
 remote_name = 'sample-10BT'
 shard_size = int(1e8) # 100M tokens per shard
 
-# create the local directory for data shards
-DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
-os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SHARDS_CACHE_DIR = os.path.join(BASE_DIR, shards_dir)
+os.makedirs(SHARDS_CACHE_DIR, exist_ok=True)
+ENCODINGS_CACHE_DIR = os.path.join(BASE_DIR, encodings_dir)
 
 # download the dataset
-fw = load_dataset('HuggingFaceFW/fineweb-edu', name=remote_name, split='train')
+fw = load_dataset('HuggingFaceFW/fineweb-edu', name=remote_name, split='train', streaming=True)
 
 # tokenization
 encodings = tiktoken.get_encoding('gpt2')
 eot_tkn = encodings._special_tokens['<|endoftext|>']
 
+filepath = os.path.join(ENCODINGS_CACHE_DIR, 'gpt2_seed_encodings.pkl')
+with open(filepath, "rb") as f:
+    seed_encodings: torch.Tensor = pickle.load(f)
+
 def tokenize(doc):
     tokens = [eot_tkn] 
     tokens.extend(encodings.encode_ordinary(doc['text']))
-    tokens_np = np.array(tokens)
-    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), 'Token dictionary tokens must be in range [0, 2^16)'
-    tokens_np_uint16 = tokens_np.astype(np.uint16)
-    return tokens_np_uint16
+
+    seed_tokens = seed_encodings[tokens].tolist()
+    seed_tokens_np = np.array(seed_tokens)
+
+    assert (0 <= seed_tokens_np).all() and (seed_tokens_np < 2**16).all(), 'Token dictionary tokens must be in range [0, 2^16)'
+    seed_tokens_np_uint16 = seed_tokens_np.astype(np.uint16)
+    return seed_tokens_np_uint16
 
 def write_datafile(filename, tokens_np):
     with open(filename, 'wb') as f:
@@ -62,7 +73,7 @@ with mp.Pool(nprocs) as pool:
             progress_bar.update(len(tokens))
         else:
             split = 'val' if shard_index == 0 else 'train'
-            filename = os.path.join(DATA_CACHE_DIR, f'fineweb_{split}_{shard_index:06d}.npy')
+            filename = os.path.join(SHARDS_CACHE_DIR, f'fineweb_{split}_{shard_index:06d}.npy')
 
             remainder = shard_size - token_count
             progress_bar.update(remainder)
@@ -75,5 +86,5 @@ with mp.Pool(nprocs) as pool:
 
     if token_count != 0:
         split = 'val' if shard_index == 0 else 'train'
-        filename = os.path.join(DATA_CACHE_DIR, f'fineweb_{split}_{shard_index:06d}.npy')
+        filename = os.path.join(SHARDS_CACHE_DIR, f'fineweb_{split}_{shard_index:06d}.npy')
         write_datafile(filename, all_tokens_np[:token_count])
